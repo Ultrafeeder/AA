68b1b0a76fe1bd601199e892f7f40ca03133704e
Ultrafeeder <jakarimstratton@gmail.com>
2022-02-20 18:11:36 -0500

    trying

--- openLibraryAPI.mjs
+++ openLibraryAPI.mjs
@@ -4,8 +4,7 @@ import bodyParser from "body-parser";
 const app = express();
 // const nodeFetch = require('node-fetch')
 
-let response = fetch("http://openlibrary.org/search.json?q=the+giver")
-    .then(response => response.json()
+let response = fetch("http://openlibrary.org/search.json?q=the+giver").then(response => response.json()
         .then(data => console.log(data)))
 
     console.log(response)
